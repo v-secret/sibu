33977499d7db2e4e5224911cf2482eda156d7d4a
v-secret <77598415+v-secret@users.noreply.github.com>
2021-01-18 11:23:01 +0800

    Delete showlist.js

--- showlist.js
+++
@@ -1,6 +0,0 @@
-const selectElement = (element) => document.querySelector(element);
-
-selectElement('.list-1').addEventListener('click', () => {
-	document.selectElementById('footer-list-1').classList.toggle('showlist1');
-
-});
